6c3d41d1d6e101cb05e7c1bf799299f79894ef7c
nikhilvenkatkumsetty <43575864+nikhilvenkatkumsetty@users.noreply.github.com>
2020-10-24 16:50:04 +0530

    Update FizzBuzzTC.java

--- FizzBuzz/src/FizzBuzzTC.java
+++ FizzBuzz/src/FizzBuzzTC.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class FizzBuzzTC {
 	
 	@Test
-	public void testFizzBuzznormalnumbers() {
+	public void testFizzBuzzthatreturns1() {
 		String result = Main.FizzBuzz(1);
 		assertEquals("1",result);
 	}
@@ -14,5 +14,11 @@ public class FizzBuzzTC {
 	public void testFizzBuzzthatreturnsFizz() {
 		String result = Main.FizzBuzz(3);
 		assertEquals("Fizz",result);
+	}
+	
+	@Test
+	public void testFizzBuzzthatreturns2() {
+		String result = Main.FizzBuzz(2);
+		assertEquals("2",result);
 	}
 }
