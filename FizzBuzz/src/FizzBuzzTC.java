f583ef43bf4c4d748d22717ee59d6a5f1c6aae06
nikhilvenkatkumsetty <43575864+nikhilvenkatkumsetty@users.noreply.github.com>
2020-10-24 16:40:10 +0530

    Update FizzBuzzTC.java

--- FizzBuzz/src/FizzBuzzTC.java
+++ FizzBuzz/src/FizzBuzzTC.java
@@ -9,4 +9,10 @@ public class FizzBuzzTC {
 		String result = Main.FizzBuzz(1);
 		assertEquals("1",result);
 	}
+	
+	@Test
+	public void testFizzBuzzthatreturnsFizz() {
+		String result = Main.FizzBuzz(3);
+		assertEquals("Fizz",result);
+	}
 }
