2d6b34ac8be0be2394c6ac11666b651c9bb94aa4
nikhilvenkatkumsetty <43575864+nikhilvenkatkumsetty@users.noreply.github.com>
2020-10-24 16:32:00 +0530

    Add files via upload

---
+++ FizzBuzz/src/FizzBuzzTC.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FizzBuzzTC {
+	
+	@Test
+	public void testFizzBuzznormalnumbers() {
+		String result = Main.FizzBuzz(1);
+		assertEquals("1",result);
+	}
+}
---
+++ FizzBuzz/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static String FizzBuzz(int i) {
+		
+		return null;
+	}
+	
+}
